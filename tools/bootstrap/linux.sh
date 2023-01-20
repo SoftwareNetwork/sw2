3298d056a5eb39dfceb1039cc3ea7a1a85a35f3e
Egor Pugin <egor.pugin@gmail.com>
2023-01-20 22:37:48 +0300

    Update bootstrap commands.

--- tools/bootstrap/linux.sh
+++ tools/bootstrap/linux.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-g++ -std=c++2b src/*.cpp -Isrc -o sw
+g++ -std=c++2b src/*.cpp -o sw
+
+# some other variants
+# g++ src/*.cpp -std=c++2b -O3 -flto -fomit-frame-pointer -static -fvisibility=hidden -o sw
+# strip -s sw
--- tools/bootstrap/macos.sh
+++ tools/bootstrap/macos.sh
@@ -3,4 +3,4 @@
 # using brew
 
 export PATH="/usr/local/opt/gcc/bin:$PATH"
-g++-12 -std=c++2b src/*.cpp -Isrc -I/usr/local/opt/fmt/include -o sw
+g++-12 -std=c++2b src/*.cpp -I/usr/local/opt/fmt/include -o sw
